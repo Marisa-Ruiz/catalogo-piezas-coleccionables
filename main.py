923db1946b17d573d8dc0b9041ab8df8274e8903
Marisa-Ruiz <marisa120685@gmail.com>
2026-09-18 12:49:47 +0200

    refactor: change price input data type to float

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ print("======================================")
 
 id_pieza = input("Introduce el ID de la pieza: ")
 nombre_pieza = input("Introduce el nombre del pieza: ")
-precio = input("Introduce el precio del pieza: ")
+precio = float(input("Introduce el precio del pieza: "))
 categoria = input("Introduce la categoria del pieza: ")
 estado = input("Introduce el estado del pieza: ")
 descripcion = input("Introduce la descripcion del pieza: ")
