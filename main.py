cd11b792e3cd11b5f26d71cdf482a651c2045a31
Marisa-Ruiz <marisa120685@gmail.com>
2026-09-18 11:45:50 +0200

    eat: initialize catalog and welcome message

---
+++ main.py
@@ -0,0 +1,7 @@
+catalog = []
+categorias_unicas = set()
+
+print("======================================")
+print("BIENVENIDO AL SISTEMA DE CATÁLOGO V1.0")
+print("======================================")
+
