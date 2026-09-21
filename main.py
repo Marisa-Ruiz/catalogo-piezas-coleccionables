6126f5fbcb65c8b8e5b08650c76a45b13b3f2cea
Marisa-Ruiz <marisa120685@gmail.com>
2026-09-21 20:05:05 +0200

    feat: capture 10 items in a loop and store them in catalog

--- main.py
+++ main.py
@@ -5,20 +5,21 @@ print("======================================")
 print("BIENVENIDO AL SISTEMA DE CATÁLOGO V1.0")
 print("======================================")
 
-item_id = input("Introduce el ID de la pieza: ")
-name = input("Introduce el nombre del pieza: ")
-price = float(input("Introduce el precio del pieza: "))
+for i in range(10):
+    item_id = input("Introduce el ID de la pieza: ")
+    name = input("Introduce el nombre del pieza: ")
+    price = float(input("Introduce el precio del pieza: "))
+    category = input("Introduce la categoria del pieza: ")
+    status = input("Introduce el estado del pieza: ")
+    description = input("Introduce la descripcion del pieza: ")
 
-print(type(price))
+    item = {
+        "id": item_id,
+        "name": name,
+        "price": price,
+        "category": category,
+        "status": status,
+        "description": description
+    }
 
-#category = input("Introduce la categoria del pieza: ")
-#status = input("Introduce el estado del pieza: ")
-#description = input("Introduce la descripcion del pieza: ")
-
-item = {
-    "id": item_id,
-    "name": name,
-    "price": price
-}
-
-print(item)
+    catalog.append(item)
